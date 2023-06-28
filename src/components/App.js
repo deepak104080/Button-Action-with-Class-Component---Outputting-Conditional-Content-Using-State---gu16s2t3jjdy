d87ccfc29915f5d8689c0751237040b531c9478f
deepak104080 <deepak104080@gmail.com>
2023-06-28 21:55:53 +0530

    Asg

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,22 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
+    constructor() {
+		super();
+		this.state = {
+			status: false
+		}
 	};
+
+	// componentDidUpdate() {
+	// 	console.log(this.state)
+	// }
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				{this.state.status && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				<button id="click" onClick={() => this.setState({status: true})}>Show</button>
     		</div>
     	);
     }
